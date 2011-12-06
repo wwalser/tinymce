9d50feb2bc5f5ceb91f9bc60619822e6e18abd44
Wesley Walser <wwalser@atlassian.com>
2011-12-06 19:47:44 +1100

    Initial development of the Deferred Plugins Plugin for TinyMCE

---
+++ jscripts/tiny_mce/plugins/deferredplugins/editor_plugin_src.js
@@ -0,0 +1,41 @@
+/**
+ * editor_plugin_src.js
+ *
+ * Copyright 2011, Wesley Walser
+ * Released under LGPL License.
+ */
+
+(function() {
+	tinymce.create('tinymce.plugins.DeferredPluginsPlugin', {
+		/**
+		 * Initializes the plugin, this will be executed after the plugin has been created.
+		 * This call is done before the editor instance has finished it's initialization so use the onInit event
+		 * of the editor instance to intercept that event.
+		 *
+		 * @param {tinymce.Editor} ed Editor instance that the plugin is initialized in.
+		 * @param {string} url Absolute URL to where the plugin is located.
+		 */
+		
+		init : function(ed, url) {
+		},
+
+		/**
+		* Returns information about the plugin as a name/value array.
+		* The current keys are longname, author, authorurl, infourl and version.
+		*
+		* @return {Object} Name/value array containing information about the plugin.
+		*/
+		getInfo : function() {
+			return {
+				longname : 'DeferredPlugins',
+				author : 'Wesley Walser',
+				authorurl : 'http://github.com/wwalser',
+				infourl : 'http://github.com/wwalser/tinymce',
+				version : 0
+			};
+		}
+	});
+
+	// Register plugin
+	tinymce.PluginManager.add('deferredplugins', tinymce.plugins.DeferredPluginsPlugin);
+})();
